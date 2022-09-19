28ad3b2f18dae5fade73e4b8e4b1bbc376168f15
L_DelOff <51275636+LDelOff@users.noreply.github.com>
2022-09-19 23:05:10 +0300

    Убрал тестовую версию

--- src/main/java/org/ldeloff/pp_3_1_1/dao/UserDAO.java
+++ src/main/java/org/ldeloff/pp_3_1_1/dao/UserDAO.java
@@ -1,11 +1,10 @@
 package org.ldeloff.pp_3_1_1.dao;
 
 import org.ldeloff.pp_3_1_1.model.User;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface UserDAO extends PagingAndSortingRepository<User, Long> {
+public interface UserDAO{
     List<User> allUser();
     void add(User user);
     void delete(long id);
--- src/main/java/org/ldeloff/pp_3_1_1/dao/UserDAOImpl.java
+++ src/main/java/org/ldeloff/pp_3_1_1/dao/UserDAOImpl.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
-    @PersistenceContext
+    @PersistenceContext(type=javax.persistence.PersistenceContextType.EXTENDED)
     private EntityManager entityManager;
 
     @Override
@@ -41,70 +41,5 @@ public class UserDAOImpl implements UserDAO {
     @Override
     public User getById(long id) {
         return entityManager.find(User.class, id);
-    }
-
-    @Override
-    public Iterable<User> findAll(Sort sort) {
-        return null;
-    }
-
-    @Override
-    public Page<User> findAll(Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public <S extends User> S save(S s) {
-        return null;
-    }
-
-    @Override
-    public <S extends User> Iterable<S> saveAll(Iterable<S> iterable) {
-        return null;
-    }
-
-    @Override
-    public Optional<User> findById(Long aLong) {
-        return Optional.empty();
-    }
-
-    @Override
-    public boolean existsById(Long aLong) {
-        return false;
-    }
-
-    @Override
-    public Iterable<User> findAll() {
-        return null;
-    }
-
-    @Override
-    public Iterable<User> findAllById(Iterable<Long> iterable) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void deleteById(Long aLong) {
-
-    }
-
-    @Override
-    public void delete(User user) {
-
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends User> iterable) {
-
-    }
-
-    @Override
-    public void deleteAll() {
-
     }
 }
